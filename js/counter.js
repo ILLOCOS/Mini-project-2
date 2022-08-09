0985271e8abd747e676f8f8ecc7feef325b491b6
MelPinDel <melpdel@gmail.com>
2022-08-10 01:36:30 +0800

    added new svg files, updated banner

--- js/counter.js
+++ js/counter.js
@@ -7,43 +7,51 @@ var observer = new IntersectionObserver(function(entries) {
 observer.observe(document.querySelector("#counter"));
 
 const counter = () => {
-    var counter1 = 00;
-    var counter2 = 00;
-    var counter3 = 00;
+    var counter1 = Number(document.getElementById("counter1").innerHTML)
+    var counter2 = Number(document.getElementById("counter1").innerHTML)
+    var counter3 = Number(document.getElementById("counter1").innerHTML)
     var appendCounter1 = document.getElementById("counter1");
     var appendCounter2 = document.getElementById("counter2");
     var appendCounter3 = document.getElementById("counter3");
     var Interval;
 
-    clearInterval(Interval);
-    Interval = setInterval(startTimer, 20);
-
-    function startTimer() {
-        counter1+=50;
-        counter2++;
-        counter3+=50;
-
-        if (counter1 <= 2500) {
-            appendCounter1.innerHTML = counter1;
-        } else {
-            appendCounter1.innerHTML = 2500;
-        }
-
-        if (counter2 <= 150) {
+    if (counter1 != 0) {
+        clearInterval(Interval)
+    } else {
+        var counter1 = 00;
+        var counter2 = 00;
+        var counter3 = 00;
+        
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 20);
+    
+        function startTimer() {
+            counter1+=50;
             counter2++;
-            appendCounter2.innerHTML = counter2;
-        } else {
-            appendCounter2.innerHTML = 150;
-        }
-
-        if (counter3 <= 2500) {
-            appendCounter3.innerHTML = counter3;
-        } else {
-            appendCounter3.innerHTML = 2500;
-        }
-
-        if (counter1 == 2500 && counter2 == 150 && counter3 == 2500) {
-            clearInterval(Interval);   
-        }
+            counter3+=50;
+    
+            if (counter1 <= 2500) {
+                appendCounter1.innerHTML = counter1;
+            } else {
+                appendCounter1.innerHTML = 2500;
+            }
+    
+            if (counter2 <= 150) {
+                counter2++;
+                appendCounter2.innerHTML = counter2;
+            } else {
+                appendCounter2.innerHTML = 150;
+            }
+    
+            if (counter3 <= 2500) {
+                appendCounter3.innerHTML = counter3;
+            } else {
+                appendCounter3.innerHTML = 2500;
+            }
+    
+            if (counter1 == 2500 && counter2 == 150 && counter3 == 2500) {
+                clearInterval(Interval);   
+            }
+        };
     };
 };
